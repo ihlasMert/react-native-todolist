89688aa69d6a4b1fedd73363801b923cc23e11e5
ihlasMert <ihlas1mert@gmail.com>
2023-05-21 12:03:58 +0300

    added delete function

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, View, FlatList } from "react-native";
 
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
@@ -14,6 +14,11 @@ export default function App() {
     ]);
   }
 
+  function deleteFunction(id) {
+    setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+  }
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
@@ -21,7 +26,13 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDelete={deleteFunction}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
             return item.id;
